05f4d0248c5a1cc646045329a618489ff1966628
Najmul Rusu <nhrusu@gmail.com>
2022-02-17 15:02:23 +0600

    first error msg added

--- js/accounting.js
+++ js/accounting.js
@@ -1,22 +1,58 @@
 function getIncome() {
     const incomeField = document.getElementById('income-field');
-    let getIncomeField = parseFloat(incomeField.value);
-    return getIncomeField;
+    let getIncomeFieldText = incomeField.value;
+    let getTotalIncome = parseFloat(getIncomeFieldText);
+    return getTotalIncome;
 };
 
-document.getElementById('calculate-button').addEventListener('click', function() {
+function catchBalance() {
+    // get food cost
     const foodField = document.getElementById('food-field');
-    const getFoodField = foodField.value;
+    const getFoodFieldText = foodField.value;
+    const foodCost = parseFloat(getFoodFieldText);
+    // get rent cost
     const rentField = document.getElementById('rent-field');
-    const getRentField = rentField.value;
+    const getRentFieldText = rentField.value;
+    const rentCost = parseFloat(getRentFieldText);
+    // get clothing cost
     const clothesField = document.getElementById('clothes-field');
-    const getClothesField = clothesField.value;
+    const getClothesFieldText = clothesField.value;
+    const clothingCost = parseFloat(getClothesFieldText);
 
-    //Calculate Expense
+    //Calculate Total Expense Amount
+    const errorMsg = document.getElementById('error-msg');
+    if (foodCost < 0 || rentCost < 0 || clothingCost < 0) {
+        errorMsg.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+    }
+    totalExpensesAmount = foodCost + rentCost + clothingCost;
     const expensesAmount = document.getElementById('expenses-amount');
-    expensesAmount.innerText = parseFloat(getFoodField) + parseFloat(getRentField) + parseFloat(getClothesField);
+    // const expensesAmountText = expensesAmount.innerText;
+    expensesAmount.innerText = totalExpensesAmount;
 
     //Calculate Balance
-    const remainingBalance = document.getElementById('balance-amount');
-    remainingBalance.innerText = getIncome() - expensesAmount.innerText;
+    const Balance = document.getElementById('total-balance');
+    const totalBalance = Balance.innerText;
+    Balance.innerText = getIncome() - totalExpensesAmount;
+    return totalBalance;
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function() {
+    catchBalance();
+})
+
+//Add Click Handler to save button
+document.getElementById('save-btn').addEventListener('click', function() {
+    const saveField = document.getElementById('saving-field');
+    const getSaveFieldText = saveField.value;
+    const savaing = parseFloat(getSaveFieldText)
+    const savingAmmount = document.getElementById('saving-ammount');
+    // let savingAmmountText = savingAmmount.innerText;
+    savingAmmount.innerText = (savaing / 100) * getIncome();
+
+    //Calculate Net Remaining Balance
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = catchBalance() - savingAmmount.innerText;
+    netRemainingBalance = remainingBalance.innerText;
 });
